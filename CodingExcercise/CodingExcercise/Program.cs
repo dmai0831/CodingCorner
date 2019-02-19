ab7bacc0f324bddbc3875d490f4068f7e4ceb802
dmai <dmai0831@gmail.com>
2019-02-18 20:39:31 -0500

    First Unique Character in a String

--- CodingExcercise/CodingExcercise/Program.cs
+++ CodingExcercise/CodingExcercise/Program.cs
@@ -10,6 +10,36 @@ namespace CodingExcercise
     {
         static void Main(string[] args)
         {
+            
+        }
+        
+        /// <summary>
+        /// First Unique Character in a String 
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns>character index</returns>
+        public static int FindFirstUniqueCharacter(String str)
+        {
+            /*
+            Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+            Examples:
+            s = "leetcode"
+            return 0.
+
+            s = "loveleetcode",
+            return 2.
+
+            Note: You may assume the string contain only lowercase letters.
+            */
+            int index = -1;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str.IndexOf(str[i]) == str.LastIndexOf(str[i]))
+                {
+                    return i;
+                }
+            }
+            return index;
         }
         
     }
